0e9e98488f39cf5e313c5fd094a13e9c5d2eeb8f
ss889 <98911705+ss889@users.noreply.github.com>
2025-03-13 22:36:02 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import Scatterplot from './Scatterplot';
 import GroupedBarChart from './GroupedBarChart';
+import GroupedBarChartPopulation from './GroupedBarChartPopulation'; // Import the new component
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <h1>Insurance Rates vs. Crash Rates</h1>
-      //<Scatterplot />
-      //<GroupedBarChart />
+      <Scatterplot />
+      <GroupedBarChart />
+
+      <h1>Insurance Rates vs. Population Density</h1>
+      <GroupedBarChartPopulation /> {/* Add the new component */}
     </div>
   );
 }
